9dce51e41bada7f185806069090b9adfcab19e02
Tslink <47263479+Tslink@users.noreply.github.com>
2019-08-28 17:30:12 -0400

    Add files via upload
    Completed Graph 2

---
+++ plot2.R
@@ -0,0 +1,29 @@
+# Read file into R
+
+library(lubridate)
+library(dplyr)
+##get information from the flat file
+df <- read.table("C:\\Users\\thomas.myslinski\\Documents\\household_power_consumption.txt",header = TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+##convert the data information to the correct date format
+dt <- strptime(paste(df$Date, df$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+df <- cbind(df, dt)
+
+df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
+
+##Filter out the data needed for the graph
+dfwork <- df %>%
+  select(Date, Time, Global_active_power, Global_reactive_power, Voltage, Global_intensity, Sub_metering_1, Sub_metering_2, Sub_metering_3,dt) %>%
+  filter(Date >= "2007-02-01" & Date <= "2007-02-02")
+
+
+
+##Set the device to output the graph to a PNG file
+png("plot2.png", height = 480, width = 480)
+
+##Create the graph
+plot(dfwork$dt, as.numeric(dfwork$Global_active_power), type = "l",xlab = "" ,ylab = "Global Active Power(kilowatts)")
+
+
+##close the PNG device
+dev.off()
